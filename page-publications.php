32142bf5068bfcb92e48d956315d325fc7d3540a
Niki Brown <nikibrown@Niki-Browns-MacBook-Pro.local>
2021-05-04 13:24:07 -0700

    publications page first pass

---
+++ page-publications.php
@@ -0,0 +1,201 @@
+<?php
+/**
+ * Template Name: Publications 
+ *
+ * This is the template that displays all pages by default.
+ * Please note that this is the WordPress construct of pages
+ * and that other 'pages' on your WordPress site may use a
+ * different template.
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-hierarchy/
+ *
+ * @package prime
+ */
+
+ ?>
+
+<?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
+
+<?php get_header();?>
+
+<main>
+    <div class="container">
+		<article>
+			<div class="row">
+				<div class="col-lg-12">
+				
+					<?php if( get_field('lead_headline') ): ?>
+						<div class="lead-section">
+							<p class="lead">
+								<?php the_field("lead_headline"); ?>
+							</p>
+
+							<?php if( get_field('lead_intro_text') ): ?>
+								<?php the_field("lead_intro_text"); ?>
+							<?php endif; ?>
+						</div>
+					<?php endif; ?>
+				</div>
+			</div>
+		<?php endwhile; endif; ?>
+
+			<div class="row">
+				<div class="col-lg-4">
+					<select class="custom-select custom-select-lg mb-3 select-filter1">
+						<!-- Loop through custom taxonomy terms -->
+						<option value="filter-area-all">View by Research Area</option>
+						<option value="filter-all">All Research Areas</option>
+						<option value="filter-physiology">Physiology</option>
+						<option value="filter-ecology">Ecology</option>
+						<option value="filter-behavior">Behavior</option>
+						<option value="filter-modeling">Modeling</option>
+					</select>
+				</div>
+				<div class="col-lg-4">
+					<select class="custom-select custom-select-lg mb-3 select-filter2">
+						<option value="filter-all">View by Author</option>
+						<option value="filter-all">All Authors</option>
+
+						<?php $args = array(
+							'post_type' => array( 'profiles' ),
+							'numberposts' => -1,	
+							'orderby'        => 'title',
+							'order'          => 'DESC'
+						);
+
+						// The Query
+						$query = new WP_Query( $args ); ?>
+
+						<?php if ( $query->have_posts() ) : while ( $query->have_posts() ) : $query->the_post(); ?>
+
+						<?php 
+							$name = get_the_title();
+							$nameclass = strtolower(str_replace(' ', '', $name));
+						?>
+
+							<option value="filter-author-<?php echo $nameclass; ?>"><?php the_title(); ?></option>
+							
+						<?php endwhile; endif; ?>
+						<?php wp_reset_postdata(); ?>
+
+
+					</select>
+				</div>
+				<div class="col-lg-4">
+					
+				</div>
+			</div>
+			
+			<div class="row">
+				<div class="col-lg-12">
+
+					<!-- TODO: somehow list year only once -->
+
+					<!-- <h3><span class="year">2021</span></h3> -->
+
+					<ul class="publications list-unstyled">
+			
+						<?php $args = array(
+							'post_type' => array( 'publications' ),
+							'numberposts' => -1,	
+							'meta_key'       => 'publication_date',
+							'orderby'        => 'meta_value_num',
+							'order'          => 'DESC'
+						);
+
+						// The Query
+						$query = new WP_Query( $args ); ?>
+
+						<?php if ( $query->have_posts() ) : while ( $query->have_posts() ) : $query->the_post(); ?>
+
+						
+
+						<h3><span class="year"><?php the_field("year"); ?></span></h3>
+							<li class="publication-single
+						
+								<?php foreach ( get_the_terms( get_the_ID(), 'research_area' ) as $tax ) {
+									echo 'filter-' . strtolower($tax->name); 
+								} ?>
+								
+								<?php if( have_rows('principal_authors') ): while( have_rows('principal_authors') ): the_row();  ?>
+
+								<?php $principle_author = get_sub_field('principle_author'); 
+
+									if( $principle_author ): 
+										$principle_author_title = $principle_author['title'];
+										$nameclass = strtolower(str_replace(' ', '', $principle_author_title));
+					
+									?>
+								
+									filter-author-<?php echo $nameclass; ?>
+
+									<?php endif; ?>
+
+								<?php endwhile;  endif; ?>
+								
+								filter-all
+							">
+								<h3><a href="<?php the_field("publication_url"); ?>"><?php the_title(); ?></a></h3>
+								
+
+								<?php the_field("publication_file"); ?>
+								
+								<p><em><?php the_field("publication_date"); ?>, </em><?php the_field("journal_name"); ?></p>
+
+								<!-- TODO: WYSIWYG for now - cant get relationship query to display inside wp_query -->
+
+								<div class="authors">
+									<?php if( have_rows('principal_authors') ): while( have_rows('principal_authors') ): the_row();  ?>
+
+									<?php $principle_author = get_sub_field('principle_author'); 
+
+										if( $principle_author ): 
+											$principle_author_url = $principle_author['url'];
+											$principle_author_title = $principle_author['title'];
+						
+										?>
+											<span><a  href="<?php echo esc_url( $principle_author_url ); ?>"><?php echo esc_html( $principle_author_title ); ?></a>, </span> 
+										<?php endif; ?>
+
+
+								 	<?php endwhile;  endif; ?>
+
+									<?php the_field("authors"); ?>
+								</div>
+
+								
+								
+
+								<p><strong>Research Area: </strong> 
+								<?php foreach ( get_the_terms( get_the_ID(), 'research_area' ) as $tax ) {
+									echo '<span>' . __( $tax->name ) . '</lspan';
+								} ?></p>
+
+								<?php $post_id = get_the_ID(); ?>
+
+								<p class="abstract-link" data-toggle="collapse" data-target="#abstract<?php echo $post_id; ?>">
+                                    Abstract <i class="fas fa-caret-right"></i>
+                                </p>
+
+								<div class="abstract-details collapse" id="abstract<?php echo $post_id; ?>">
+									<?php the_field("abstract"); ?>
+								</div>
+							</li>
+					
+					
+						<?php endwhile; endif; ?>
+						<?php wp_reset_postdata(); ?>
+
+					</ul>
+				
+				</div>
+
+			</div>
+		</div>
+	</article>
+</main>
+	
+
+
+	
+<?php get_footer(); ?>
