deccd160b449860a11a52952fdb5d597da4b6133
Niki Brown <niki.brown@dronedeploy.com>
2021-08-30 08:43:43 -0400

    equal height team cards

--- page-team.php
+++ page-team.php
@@ -18,15 +18,19 @@
 
 <?php get_header();?>
 
-<section class="hero section-md" style="background-image: url('https://nikibrown.github.io/fffibers/assets/img/hero.jpg');">
-	<div class="container">
-		<div class="row align-items-end">
-			<div class="col-lg-12">
-				<h1>General Subpage</h1>
+<?php if( get_field('header_image') ) { ?>
+	<section class="hero section-md" style="background-image: url('<?php the_field("header_image"); ?>');">
+<? } else { ?>
+	<section class="hero section-md" style="background-image: url('<?php the_field("default_sub_page_header_image", "option"); ?>');">
+<? } ?>
+		<div class="container">
+			<div class="row align-items-end">
+				<div class="col-lg-12">
+					<h1><?php the_title(); ?></h1>
+				</div>
 			</div>
 		</div>
-	</div>
-</section>
+	</section>
 
 <?php get_template_part( 'template-parts/breadcrumbs'); ?>
 
@@ -41,53 +45,37 @@
 			<article class="offset-lg-1 col-lg-8">
 				<p class="lead"><?php the_field("intro_text"); ?></p>
 
+					<?php the_content(); ?>
+		
+					<?php
+						$counter = 1;
+						$args = array(
+							'post_parent' => $post->ID,
+							'post_type' => 'page',
+							'orderby' => 'menu_order'
+						);
 
-						<?php the_content(); ?>
-			
-						<?php
-							$counter = 1;
-							$args = array(
-								'post_parent' => $post->ID,
-								'post_type' => 'page',
-								'orderby' => 'menu_order'
-							);
-
-							$child_query = new WP_Query( $args );
-						?>
-						<div class="row">
-							<?php while ( $child_query->have_posts() ) : $child_query->the_post(); ?>
-							
-								<div class="col-lg-4 col-sm-12">
-									<div class="card card-bio">
-										<div class="card-body">
-											<a href="<?php the_permalink(); ?>"><img src="<?php the_field("headshot"); ?>" class="img-fluid" alt="<?php the_title(); ?><"></a>
-											<h5 class="card-title"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h5>
-											<p class="card-text"><?php the_field("title_or_role"); ?></p>
-										</div>
+						$child_query = new WP_Query( $args );
+					?>
+					<div class="row align-items-stretch">
+						<?php while ( $child_query->have_posts() ) : $child_query->the_post(); ?>
+						
+							<div class="col-lg-4 col-sm-12 mb-4">
+								<div class="card card-bio h-100">
+									<div class="card-body">
+										<a href="<?php the_permalink(); ?>"><img src="<?php the_field("headshot"); ?>" class="img-fluid" alt="<?php the_title(); ?><"></a>
+										<h5 class="card-title"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h5>
+										<p class="card-text"><?php the_field("title_or_role"); ?></p>
 									</div>
 								</div>
-								
-						<?php if ($counter % 3 == 0) { ?>
-							</div><!--/.row-->
-							<div class="row">
-						<?php } ?>
-
-							<?php $counter++; ?>
-						<?php endwhile; ?>
-
-					</div><!--/.row-->
-
-
-
-
-
-
-					
-					
-
-				
-
-
+							</div>
+					<?php if ($counter % 3 == 0) { ?>
+						</div><!--/.row-->
+						<div class="row align-items-stretch">
+					<?php } ?>
+						<?php $counter++; ?>
+					<?php endwhile; ?>
+				</div><!--/.row-->
 			</article>
 		</div>
 	</div>
